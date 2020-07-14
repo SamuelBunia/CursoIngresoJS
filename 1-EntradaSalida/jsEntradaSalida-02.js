4d2100a6d60ebc556ae5f8395295ca9bf76bac6e
SamuelBunia <sambunia17@gmail.com>
2020-07-14 08:24:53 -0300

    Clase 1 Programacion

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,23 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	/*let nombre = "Samuel"; 
+	tambien se puede usar let=var son los mismos
+	declaré la variable e Inicie que su valor es Samuel
+	let nombre;
+	nombre = "Samuel";
+
+	alert(nombre);
+	pasa un alert con samuel y despues como volvi a aclarar
+	la variable como raom tira despues ramon 
+	nombre ="Ramon";
+
+	alert(nombre);*/
+	/*La variable va a ser un LET y declaro la variable con el pront del dato que el usuario ingrese*/ 
+	let nombre;
+	
+	nombre = prompt("Ingrese nombre");
+
+	alert(nombre);
 }
 
